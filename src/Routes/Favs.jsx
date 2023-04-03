c2df53afed7cd84aee655715d672377e8d986e8f
Jonathan Vargas Ríos <jo.vargasrios@hotmail.com>
2023-04-03 17:31:59 -0500

    Segundo commit

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -10,7 +10,7 @@ const Favs = () => {
       <h1>Dentists Favs</h1>
       <div className="card-grid">
         {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
+        {/* Deberan renderizar una Card por cada uno de ellos........ */}
       </div>
     </>
   );
